170578e89bb8333e26e9f5ea15a85fde25576a77
Zoro <jagbetuyi001@gmail.com>
2025-04-23 18:49:43 +0100

    Order list WIP

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -1,10 +1,30 @@
 import { StyleSheet, Text, FlatList, ActivityIndicator } from "react-native";
+import { useEffect } from "react";
+import { supabase } from "@/lib/supabase";
 
 import OrderListItem from "@/components/OrderListItem";
 import { useAdminOrderList } from "@/api/orders";
 
 const OrdersScreen = () => {
   const { orders, error, isLoading } = useAdminOrderList({ archived: false });
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("custom-insert-channel")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "orders" },
+        (payload) => {
+          console.log("Change received!", payload);
+        }
+      )
+      .subscribe();
+
+    // Clean up the subscription when component unmounts
+    return () => {
+      channel.unsubscribe();
+    };
+  }, []);
 
   if (isLoading) {
     return <ActivityIndicator />;
